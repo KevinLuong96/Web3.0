a391798e0050cc450f2d507a333e718598cef8a5
Kevin Luong <c1-kevin@correlation-one.com>
2018-02-04 12:42:57 -0500

    added in blinking hello text animation

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+   var text = "hello!";
+   counter = 0;
+   var addText = setInterval(function() {
+       $(".intro").html(text.substring(0, counter+1));
+       counter++;
+       if(counter == text.length) {
+           clearInterval(addText)
+       }
+   }, 250);
+
+   setInterval(function () {
+       $(".blink").toggle()
+   },500);
+});
