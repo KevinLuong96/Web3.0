209c15c03ad2ace4ada31ee6c6c521cbe0538bba
Kevin Luong <c1-kevin@correlation-one.com>
2018-05-23 09:08:06 -0400

    added images and minor updates to fonts

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ export default class App extends React.Component {
                 <Hero/>
                 <About/>
                 <Work/>
-                <Contact/>
+                {/*<Contact/>*/}
             </div>
         )
     }
--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,9 @@
 import React from "react";
 
 export default class Contact extends React.Component {
-    render() {
-        return (
-            {/*<div className="contact">*/}
-                {/*<div className="contact-container">*/}
-                    {/*/!*<h1>Let's talk!</h1>*!/*/}
-                {/*</div>*/}
-            {/*</div>*/}
-        )
-    }
+    // render() {
+    //     return (
+    //         <div></div>
+    //     )
+    // }
 }
