0c805a087f7c0b532f2cf307bf9b5da47ed218bf
kevinluong96 <k5luong@uwaterloo.ca>
2018-05-25 16:12:26 -0400

    added some contact styles

--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 export default class Contact extends React.Component {
-    // render() {
-    //     return (
-    //         <div></div>
-    //     )
-    // }
+    render() {
+        return (
+            <div className="contact">
+                <h1 className="contact-header">Let's Talk!</h1>
+                <div className="contact-container">
+                    <h1>Hi</h1>
+                    <h1>Hi</h1>
+                    <h1>Hi</h1>
+                    <h1>Hi</h1>
+                </div>
+            </div>
+        )
+    }
 }
